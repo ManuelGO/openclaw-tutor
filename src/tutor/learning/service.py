97b1deac2a49893321f75c84c5868685ffa6ea67
Manuel Arvelo <manuelarvelo19@gmail.com>
2026-08-14 20:43:46 +0200

    feat: make answer evaluation persistence atomic
    : main

--- src/tutor/learning/service.py
+++ src/tutor/learning/service.py
@@ -1,16 +1,14 @@
+import json
 from uuid import uuid4
-
+from tutor.db.database import get_connection
 from tutor.learning.answers import Answer
-from tutor.learning.answers_repository import save_answer
 from tutor.learning.evaluator import evaluate, build_evaluation
 from tutor.learning.evaluations import AnswerEvaluation
-from tutor.learning.evaluations_repository import save_evaluation
 from tutor.learning.questions import Question
 from tutor.learning.questions_repository import (
     get_active_question,
     get_pending_questions,
     get_question,
-    mark_question_answered,
     mark_question_sent,
 )
 
@@ -43,10 +41,11 @@ def submit_answer(
         answer=answer,
         evaluation=generated_evaluation,
     )
-
-    save_answer(answer)
-    save_evaluation(evaluation)
-    mark_question_answered(question.id)
+    # All database changes happen atomically.
+    persist_answer_evaluation(
+        answer=answer,
+        evaluation=evaluation,
+    )
 
     return evaluation
 
@@ -81,3 +80,54 @@ def submit_active_answer(
         question_id=question.id,
         text=text,
     )
+def persist_answer_evaluation(
+    answer: Answer,
+    evaluation: AnswerEvaluation,
+) -> None:
+    with get_connection() as connection:
+        connection.execute(
+            """
+            INSERT INTO answers (
+                id,
+                question_id,
+                answer
+            )
+            VALUES (?, ?, ?)
+            """,
+            (
+                answer.id,
+                answer.question_id,
+                answer.answer,
+            ),
+        )
+
+        connection.execute(
+            """
+            INSERT INTO evaluations (
+                id,
+                answer_id,
+                score,
+                feedback,
+                strengths,
+                gaps
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (
+                evaluation.id,
+                evaluation.answer_id,
+                evaluation.score,
+                evaluation.feedback,
+                json.dumps(evaluation.strengths),
+                json.dumps(evaluation.gaps),
+            ),
+        )
+
+        connection.execute(
+            """
+            UPDATE questions
+            SET status = 'answered'
+            WHERE id = ?
+            """,
+            (answer.question_id,),
+        )
